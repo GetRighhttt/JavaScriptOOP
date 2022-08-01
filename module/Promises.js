bd976ef5906946272fb8a7105cdbb5d9882636f5
stefan <stefanbayne@gmail.com>
2022-08-01 19:40:39 -0400

    Promise examples.

--- module/Promises.js
+++ module/Promises.js
@@ -1,46 +1,90 @@
 /**
- * A promise in JS is code that takes some time to wait for a result. 
- * 
+ * A promise is a proxy for a value not necessarily known when the promise is created. it allows
+ * you to associate handlers with an asynchronous action's eventual sucess value or failure.
+ *
  * Promises are used for asynchronous programming.(not occurring at the same time.)
- * 
- * async programming is used when we want to free up the main thread in android to allow for 
- * computations to happen in the background. Whereas in JS, it's used to defer a job and await,
- * a response. We do this with coroutines in Android also.
- * 
- * Promises are used often when making an HTTP request to a server, and when there's too much happening
- * at one time, so we need to defer a task and await a result.
- * 
+ *
+ * async programming is used to complete multiple tasks at one time, without them happening
+ * in order. In some scenarios in programming, all of the tasks do not have to happen one
+ * after the other, or synchronously. It's better often to run multiple tasks on separate threads,
+ * or at separate times, and waiting for the job to return. It's better overall.
+ *
+ * In most scenarios, async programming is one of the most importatnt concepts to master.
+ *
  * resolve = success; reject = failure
  */
 
-
-// resolve set to 3 seconds, and reject set to 1. reject message should show.
 const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("data successfully back from the server...")
-    }, 3000)
+  setTimeout(() => {
+    resolve("data successfully back from the server...");
+  }, 3000);
 
-    setTimeout(() => {
-        reject("failed to get data back from the server...")
-    }, 1000)
-})
+  setTimeout(() => {
+    reject("failed to get data back from the server...");
+  }, 1000);
+});
 
 /**
  * Now we actually use the promise, catch the error if there is one.
- * 
+ *
  * finally is used regardless if there is an error or not.
- * 
- * this is how we initiate a promise. 
+ *
+ * this is how we initiate a promise.
  */
-promise.then(response => {
-    console.log(response)
-}).catch(error => {
-    console.log(error)
-}).finally(() => {
-    console.log("complete...")
-})
+promise
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .finally(() => {
+    console.log("complete...");
+  });
 
 /**
  failed to get data back from the server...
 complete...
  */
+
+/**
+ * 
+ * Another example: 
+ * 
+ * promises are always either pending, failed, or success.
+ * 
+ * resolve changes status of promise from pending to fulfilled.
+ * 
+ * reject changes status of promise from pending to failed.
+ */
+
+// create an instance of a promise using resolve and reject
+const newPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Successful.")
+    }, 3000)
+
+    setTimeout(() => {
+        reject("Didn't go through chief....")
+    }, 5000)
+});
+
+/**execute callback functions based on the status of the change.
+ * 
+ * then() and catch() are used as onfulfillment() and onfailure() method callbacks.
+ * 
+ * we can pass an argument to them and use another function if we want.
+ * 
+ **/
+
+const onFulfillment = (result) => {
+    console.log(result)
+    console.log("Fulfilled.")
+}
+const onFailure = (error) => {
+    console.log(error)
+    console.log("Failed.") 
+}
+
+newPromise.then(onFulfillment)
+newPromise.catch(onFailure)
