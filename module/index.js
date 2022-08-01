3e7032208aafeb7a556b4070b0756a54bde24fa9
stefan <stefanbayne@gmail.com>
2022-08-01 15:51:46 -0400

    Updated module

---
+++ module/index.js
@@ -0,0 +1,9 @@
+import Person from "./Classes"; // importing class Person from Classes.js
+
+/**
+ * Now we will show how to create objects with classes in JS.
+ */
+
+// we used a constructor so we have to pass in some values. 
+const stefan = new Person(27, "Stefan", "7'0", "M");
+console.log(stefan)
